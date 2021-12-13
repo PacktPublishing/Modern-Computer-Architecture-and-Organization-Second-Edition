b11700da3b1382dad9e3330b1172a2677718b511
Jim Ledin <jim@ledin.com>
2021-12-12 19:37:24 -0800

    Working on chapter 16

---
+++ Chapter16/Answers to Exercises/src/Ex__1_install_tensorflow.bat
@@ -0,0 +1,11 @@
+REM Ex__1_install_tensorflow.bat: Answer to Ch 16 Ex 1.
+
+REM This batch file installs TensorFlow and Matplotlib in Windows.
+REM Python must be installed (see https://www.python.org/downloads/).
+REM The Python installation directory must be in the system path.
+
+python --version
+
+pip install tensorflow
+
+pip install matplotlib
